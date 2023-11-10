2840f2d4ee9e2af500b6b2f71e6f6e3896a788ac
Vitor Duarte <duartevitor178@gmail.com>
2023-11-10 10:25:08 +0100

    feat | first exercise

---
+++ string-manipulator.js
@@ -0,0 +1,23 @@
+function stringManpipulate(str){
+    const strLowerCase = str.toLowerCase();
+    const strNoSpaces = str.trim();
+    const strLength = str.replace(/\s*/g, '').length;
+    const strContainsJS = strLowerCase.includes("js") || strLowerCase.includes("javascript");
+    let invertedString = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        let letter = str[i];
+        invertedString += letter;
+    }
+    const strFinal = `
+    original: ${str}
+    lowercase: ${strLowerCase}
+    no spaces: ${strNoSpaces}
+    length: ${strLength}
+    contains JS: ${strContainsJS}
+    inverted: ${invertedString}
+    `
+    console.log(strFinal);
+    return strFinal;
+}
+
+stringManpipulate(" Hello World! ")
